15ef9b6a9f00d68ea0fd53e3ae59a78e1df33b3e
wwc <onechoo1106@gmail.com>
2023-05-09 01:18:51 +1000

    why error keep causing?

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter,Route,Link } from "react-router-dom";
+import { Link } from "react-router-dom";//BrowserRouter,Route,
 
 function Navbar(){
     return(
